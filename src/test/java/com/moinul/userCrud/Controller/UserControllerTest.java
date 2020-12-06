4e62c915ed2d8b694a0c6c8bbce953132600b208
aang13 <mdmoinulh13@gmail.com>
2020-12-06 19:18:07 +0600

    controller updated

---
+++ src/test/java/com/moinul/userCrud/Controller/UserControllerTest.java
@@ -0,0 +1,47 @@
+package com.moinul.userCrud.Controller;
+
+import com.moinul.userCrud.DTO.ParentDTO;
+import com.moinul.userCrud.Model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    private UserController userController;
+
+
+    @Test
+    void createParentUser() {
+        ParentDTO parentDTO=new ParentDTO(null,"2st","last","State","city","street","zip","PARENT");
+        User user=userController.createParentUser(parentDTO);
+
+        assertEquals(user,);
+
+    }
+
+    @Test
+    void createChildUser() {
+    }
+
+    @Test
+    void updateParentUser() {
+    }
+
+    @Test
+    void updateChildUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getAllUser() {
+    }
+}
